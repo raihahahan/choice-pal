de058ae8b3a880090a55c21c934d23ea645d33e1
Muhd Raihan <muhammad.raihan.rizqullah.2019@vjc.sg>
2022-03-29 23:28:19 +0800

    minor styling

--- src/screens/randomScreen/RandomScreen.js
+++ src/screens/randomScreen/RandomScreen.js
@@ -1,10 +1,13 @@
 import { View, Text, StyleSheet } from "react-native";
 import React, { useContext } from "react";
+import { globalStyles } from "../../styles/globalStyles";
+import { Context as ThemeContext } from "../../context/theme/ThemeContext";
 
 export default function RandomScreen({ navigation, route }) {
+  const { state: theme } = useContext(ThemeContext);
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Hello world</Text>
+      <Text style={globalStyles[theme].text}>Random</Text>
     </View>
   );
 }
